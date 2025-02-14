dcb36289c0018d3c5e20675bc3573476188bc8d2
Dmitry Salnikov <dmitry.salnikovtyt@gmail.com>
2025-02-14 15:55:35 +0300

    Add Nelmio api

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
